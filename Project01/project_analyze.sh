ff417f4126131a0d0bb46f761c67561b94fb77a0
puy4 <puy4@mcmaster.ca>
2019-02-16 02:06:36 +0000

    update part 1

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -2,11 +2,12 @@
 
 cd ..
 
-select choice in "extensionCount" "deletingTmpFiles"; do
+select choice in "extensionCount" "deletingTmpFiles" "mergeLog"; do
 case "$choice" in
      extensionCount) echo "$choice"; find ~/CS1XA3 -type f | sed -e 's/.*\.//' | sort | uniq -c | sort -n | grep -Ei '(sh|py|html|hs|js|css)$'
      break;;
      deletingTmpFiles) echo "$choice"; git ls-files . --exclude-standard --others | grep '\.tmp$' | xargs git rm
      break;;
+     mergeLog) echo "$choice" ; git log --oneline | cut -d '' -f 1 | tee merge.log
 esac
 done
