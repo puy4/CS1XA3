4931b2c432255cf9f389aa753814f7c188c80ce1
puy4 <puy4@mcmaster.ca>
2019-04-18 17:05:00 +0000

    minor change

--- Project03/django_project/django_project/settings.py
+++ Project03/django_project/django_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app1.apps.App1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ Project03/django_project/homepage1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Homepage1Config(AppConfig):
+    name = 'homepage1'
