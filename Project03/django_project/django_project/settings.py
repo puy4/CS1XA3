13c699579bdf9720a38e8b1b0130bbf233130fdb
puy4 <puy4@mcmaster.ca>
2019-04-18 18:34:31 +0000

    github desktop issues solving

--- Project03/django_project/django_project/settings.py
+++ Project03/django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage1.apps.Homepage1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
