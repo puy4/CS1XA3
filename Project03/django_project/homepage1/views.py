abaa6cb2d1ea3dc5a5fa21cfa4bec1216896771f
puy4 <puy4@mcmaster.ca>
2019-04-18 14:41:24 -0400

    add title

--- Project03/django_project/homepage1/views.py
+++ Project03/django_project/homepage1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 
+hometitle = {'title':'A Simple Note-taking App'}
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', hometitle)
    
 # Create your views here.
