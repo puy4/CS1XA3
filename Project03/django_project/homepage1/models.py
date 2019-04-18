84f49a1e3579b01d26a2e7fa5f00aecaafc82662
puy4 <puy4@mcmaster.ca>
2019-04-18 21:21:41 +0000

    models update(database)

--- Project03/django_project/homepage1/models.py
+++ Project03/django_project/homepage1/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
-     content = models.Textfield()
-# Create your models here.
+     content = models.TextField()
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     date = models.DateTimeField(default=timezone.now)
