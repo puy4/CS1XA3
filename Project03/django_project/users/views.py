e35c881a07c31c4c64e962691dc1e68d16c1c289
puy4 <puy4@mcmaster.ca>
2019-04-20 04:29:26 +0000

    user creation

--- Project03/django_project/users/views.py
+++ Project03/django_project/users/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 def register(request):
-    form = UserCreationForm()
+    if request.method =='POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+              form.save()
+              messages.success(request, 'Account Created')
+              return redirect('home')
+    else:
+         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
