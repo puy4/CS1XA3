ca74b16afe3d403cbc210e7b3a03562be58fd771
puy4 <puy4@mcmaster.ca>
2019-04-24 05:04:57 +0000

    added linkss to buttons

--- Project03/django_project/homepage1/views.py
+++ Project03/django_project/homepage1/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from users import views as userviews
 
 hometitle = {'title':'A Simple Note-taking App'}
-
 
 
 def home(request):
@@ -11,3 +11,7 @@ def home(request):
 def notes(request):
     context = {'notes':Post.objects.all()}
     return render(request, 'notes.html', context)
+
+def notesinput(request):
+    if request.method == 'POST':
+        input = 
