f8279f0cb47435be0c78717e52ff26ef6506a27f
puy4 <puy4@mcmaster.ca>
2019-04-27 07:34:38 -0400

    Update forms.py

--- Project03/django_project/contactme/forms.py
+++ Project03/django_project/contactme/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from homepage1.models import Post
 
 class thecontact(forms.Form):
-    content = forms.CharField(widget=forms.Textarea)
+
     email = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     first_name = first_name = forms.CharField(required=True)
     title = forms.CharField(required=True)
+    content = forms.CharField(widget=forms.Textarea)
