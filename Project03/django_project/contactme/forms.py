4757b698bc3304943db8d10307c898f89920d798
puy4 <puy4@mcmaster.ca>
2019-04-27 07:13:04 -0400

    fixing typos

--- Project03/django_project/contactme/forms.py
+++ Project03/django_project/contactme/forms.py
@@ -4,9 +4,9 @@ from homepage1.models import Post
 class thecontact(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea)
     email = forms.CharField(required=True)
-    last_name = models.CharField(required=True)
-    first_name = first_name = models.CharField(required=True)
-    title = models.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    first_name = first_name = forms.CharField(required=True)
+    title = forms.CharField(required=True)
     class Meta:
         model = Contact
         fields = ['content', 'email', 'last_name','first_name','title' ]
--- Project03/django_project/contactme/models.py
+++ Project03/django_project/contactme/models.py
@@ -4,5 +4,5 @@ class Contact(models.Model):
      content = models.TextField()
      email = models.CharField(max_length=30)
      last_name = models.CharField(max_length=30)
-     first_name = first_name = models.CharField(max_length=30)
+     first_name = models.CharField(max_length=30)
      title = models.CharField(max_length=30)
