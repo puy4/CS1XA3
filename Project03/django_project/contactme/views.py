9fbbc807ca3dc49e20537fd5984b3aaa2b6a388f
puy4 <puy4@mcmaster.ca>
2019-04-27 08:03:11 -0400

    contact form

--- Project03/django_project/contactme/views.py
+++ Project03/django_project/contactme/views.py
@@ -3,12 +3,19 @@ from django import forms
 from .forms import thecontact
 import json
 
+def validateemail(request):
+    email = request.POST.get('email',none)
+    data = { include@ : Contact.objects.filter(email__contains='@')}
+    return JsonResponse(data)
+
+
 def send(request):
 
     if request.method == 'POST':
         form = thecontact(request.POST)
         if form.is_valid():
-            pass
+            form = form.cleaned_data
+
     else:
         form = thecontact()
     return render(request, 'contactme.html', {'form': form})
