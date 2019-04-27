b7f7c9fa6fb1e7f878439262e0e35ea9d0975a6a
puy4 <puy4@mcmaster.ca>
2019-04-27 07:33:56 -0400

    Update models.py

--- Project03/django_project/contactme/models.py
+++ Project03/django_project/contactme/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Contact(models.Model):
-     title = models.CharField(max_length=30)
+
      email = models.CharField(max_length=30)
      last_name = models.CharField(max_length=30)
      first_name = models.CharField(max_length=30)
+     title = models.CharField(max_length=30)
      content = models.TextField()
