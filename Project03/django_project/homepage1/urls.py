01aa7095b1e4e7508c77355f21a418c8e30335e6
puy4 <puy4@mcmaster.ca>
2019-04-26 22:52:49 -0400

    Update urls.py

--- Project03/django_project/homepage1/urls.py
+++ Project03/django_project/homepage1/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
     path('notes/',views.notes, name='notes'),
     path('addnotes/',views.uploadnotes, name='addnotes')
+    path('notes/',views.deletenotes, name='deletenotes')
 ]
