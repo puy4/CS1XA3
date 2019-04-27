cb6a5b44942b2c47919f94ce0dfaa4f2a99fe0a8
puy4 <puy4@mcmaster.ca>
2019-04-27 07:00:28 -0400

    Update views.py

--- Project03/django_project/contactme/views.py
+++ Project03/django_project/contactme/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 def send(request):
      body_unicode = request.body.decode('utf-8')
@@ -8,3 +9,10 @@ def send(request):
      email = reqDict.get('email','')
      title = reqDict.get('title','')
      content = reqDict.get('content','')
+     if request.method == 'POST':
+         form = thecontact(request.POST)
+         if form.is_valid():
+             pass
+         else:
+             form = thecontact()
+      return render(request, 'contactme.html', {'form': form})
