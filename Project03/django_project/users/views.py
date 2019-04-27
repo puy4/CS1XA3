880983f0b562adbedd8986e26224e53017ed0382
puy4 <puy4@mcmaster.ca>
2019-04-27 01:19:40 +0000

    users

--- Project03/django_project/users/views.py
+++ Project03/django_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from django.contrib import messages
-from django.contrib.auth.forms import SetPasswordForm
+
 
 def register(request):
     if request.method =='POST':
@@ -17,11 +17,11 @@ def register(request):
 
 def resetpassword(request):
     if request.method =='POST':
-        form = SetPasswordForm(reques.POST)
-        if form.is_valid():
-            form.save()
+        formm = SetPasswordForm(reques.POST)
+        if formm.is_valid():
+            formm.save()
             messsages.success(request, 'password reset successful')
             return redirect('login')
     else:
-        form = SetPasswordForm()
-    return render(request, 'passwordreset.html',{'form': form})
+        formm = SetPasswordForm()
+    return render(request, 'passwordreset.html',{'formm': formm})
