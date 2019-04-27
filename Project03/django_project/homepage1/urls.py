fc815002360ef13eb1fbe4223183df58d587320f
puy4 <puy4@mcmaster.ca>
2019-04-27 02:55:57 +0000

    typo

--- Project03/django_project/homepage1/urls.py
+++ Project03/django_project/homepage1/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('notes/',views.notes, name='notes'),
-    path('addnotes/',views.uploadnotes, name='addnotes')
-    path('notes/',views.deletenotes, name='deletenotes')
+    path('addnotes/',views.uploadnotes, name='addnotes'),
+    path('notes/',views.deletenotes, name='deletenotes'),
 ]
