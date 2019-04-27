3e85675ee4664d38eef23f1e9487128b1178a5a9
puy4 <puy4@mcmaster.ca>
2019-04-27 07:31:05 -0400

    Update views.py

--- Project03/django_project/contactme/views.py
+++ Project03/django_project/contactme/views.py
@@ -5,10 +5,10 @@ import json
 
 def send(request):
 
-     if request.method == 'POST':
-         form = thecontact(request.POST)
-         if form.is_valid():
-             pass
-         else:
-             form = thecontact()
-     return render(request, 'contactme.html', {'form': form})
+    if request.method == 'POST':
+        form = thecontact(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = thecontact()
+    return render(request, 'contactme.html', {'form': form})
