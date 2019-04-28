064c08a7d0f918ca3da6a5ec19dd3a31131537e0
puy4 <47121291+puy4@users.noreply.github.com>
2019-04-27 23:51:31 -0400

    Update views.py

--- Project03/django_project/users/views.py
+++ Project03/django_project/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+<!--
+registration.
+-->
 
 def register(request):
     if request.method =='POST':
