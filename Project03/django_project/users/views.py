4ce849694fb53ab778ba6b2aa03e16ad443af3c4
puy4 <puy4@mcmaster.ca>
2019-04-28 02:05:10 +0000

    no idea

--- Project03/django_project/users/views.py
+++ Project03/django_project/users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+import json
 
 def register(request):
-    if request.method =='POST':
-         form = UserCreationForm(request.POST)
-         if form.is_valid():
-              form.save()
+    jreq = json.loads(request.body)
+    username = jreq.get('username','')
+    password = jreq.get('password','')
+    reenterpas = jreq.get('reenterpas','')
+    if password == reenterpas and len(password) > 8 and  username !="":
+              user = User.objects.create_user(username=username, password=password)
+              user.asve()
               messages.success(request, 'Account Created, you can now log in!')
               return redirect('login')
     else:
-         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+         return HttpResponse('login fails')
+   
